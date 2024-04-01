0d099d855e1f665e1a6de043d97da5752de35b40
AlexLogvinenko5 <aleksei.logvineneko555@yandex.ru>
2024-04-01 14:47:49 +0300

    add3

---
+++ dccfzsccscfsc.py
@@ -0,0 +1,12 @@
+ M = [int(x) for x in open('17.txt')]
+A = [x for x in M if abs(x) % 100 == 17]
+count = 0
+maxi = -9999999999
+for i in range(len(M)-2):
+    x, y, z = M[i], M[i+1], M[i+2]
+    if len([i for i in [x, y, z] if len(str(abs(i))) == 4]) == 2:
+        if any(i % 5 == 0 for i in [x, y, z]):
+            if (x + y + z) > max(A):
+                count += 1
+                maxi = max(maxi, x + y + z)
+print(count, maxi)
