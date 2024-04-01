5cce723c288b2294cde33379f7abfe1154ee8362
AlexLogvinenko5 <aleksei.logvineneko555@yandex.ru>
2024-04-01 14:52:14 +0300

    add6

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
- M = [int(x) for x in open('17_13088.txt')]
-A = [x for x in M if abs(x) % 100 == 17]
-count = 0
-maxi = -9999999999
-for i in range(len(M)-2):
-    x, y, z = M[i], M[i+1], M[i+2]
-    if len([i for i in [x, y, z] if len(str(abs(i))) == 4]) == 2:
-        if any(i % 5 == 0 for i in [x, y, z]):
-            if (x + y + z) > max(A):
-                count += 1
-                maxi = max(maxi, x + y + z)
-print(count, maxi)
+f = open('17_13088.txt')
+array = [int(x) for x in f]
+n = len(array)
+mx = 0
+k =  0
+mx_sm = 0
+for i in array:
+    if i > mx and i%100 == 17:
+        mx = i
+for i in range(n-3):
+    a = array[i]
+    b = array[i+1]
+    c = array[i+2]
+    sm = a+b+c
+    if sm > mx:
+        if a%5 == 0 or b%5 == 0 or c%5 == 0:
+            n1 = len(str(a))
+            n2 = len(str(b))
+            n3 = len(str(c))
+            if (n1 == 4 and n2 == 4) or (n1 == 4 and n3 == 4) or (n2 == 4 and n3 == 4):
+                k += 1
+                if sm > mx_sm:
+                    mx_sm = sm
+print(k,mx_sm)
